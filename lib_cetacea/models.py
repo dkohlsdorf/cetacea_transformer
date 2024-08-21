95ffd86d931faed1e1fc136a3cea6ece78081545
Ubuntu <ubuntu@ip-172-31-42-217.ec2.internal>
2024-08-21 21:06:58 +0000

    adding sequences

---
+++ lib_cetacea/models.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+
+from lib_cetacea.patches import *
+from lib_cetacea.attention_layers import *
+from lib_cetacea.masking import * 
+
+
+def encoder(length, dim, n_layers, n_heads):
+  i = Input((length, dim))
+  x = LayerNormalization()(i)
+  for _ in range(n_layers):
+    x = EncoderLayer(
+      d_model = dim,
+      num_heads = n_heads,
+      dff = dim
+    )(x)
+  o = x
+  return Model(inputs = [i], outputs = [o])
+
+
+def sequence_classifier(enc, length, dim, output_dim, label_sequence=False):
+  i = Input((length, dim))
+  e = enc(i)
+  if not label_sequence:
+    e = Lambda(lambda x: K.sum(x, axis=1), output_shape=lambda s: (s[0], s[2]))(e)
+  o = FeedForward(dim, output_dim, activation='softmax')(e)
+  return Model(inputs = [i], outputs = [o])
+
